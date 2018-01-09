b73d91312aefd356db36c64724d915303e388ebf
Sadik Ay <sadikay91@gmail.com>
2018-01-09 04:32:12 +0300

    configure dev server

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -3,12 +3,17 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 Rails.application.config.content_security_policy do |p|
-  p.default_src :self, :https
-  p.font_src    :self, :https, :data
-  p.img_src     :self, :https, :data
-  p.object_src  :none
-  p.script_src  :self, :https
-  p.style_src   :self, :https, :unsafe_inline
+  if Rails.env.development?
+    p.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035'
+    p.script_src :self, :https, :unsafe_eval, :unsafe_inline
+  else
+    p.default_src :self, :https
+    p.font_src    :self, :https, :data
+    p.img_src     :self, :https, :data
+    p.object_src  :none
+    p.script_src  :self, :https
+    p.style_src   :self, :https, :unsafe_inline
+  end
 
   # Specify URI for violation reports
   # p.report_uri "/csp-violation-report-endpoint"
